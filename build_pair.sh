8f635bb21dd39a2aba649676d4038158f8f85fb0
R. G. Esteves <rgesteves@gmail.com>
2022-06-29 02:40:50 -0700

    Script to build consumable/consuming pair

---
+++ build_pair.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Builds the consumable/consuming pair
+INSTALLDIR=$(pwd)/install
+BUILDDIR=build
+rm -rf ${INSTALLDIR}
+mkdir -p ${INSTALLDIR}
+pushd consumablelib
+rm -rf ${BUILDDIR}
+cmake -S . -B ${BUILDDIR}
+cmake --build build
+cmake --install build --prefix ${INSTALLDIR}
+popd
+
+pushd consumingexe
+rm -rf ${BUILDDIR}
+cmake -S . -B ${BUILDDIR} -DCMAKE_PREFIX_PATH:STRING=${INSTALLDIR}
+cmake --build build
+popd
